f2d7a27875e015229ef9d9f20152e1c8c04f184f
Philip Jay <phil@jay.id.au>
2018-05-31 14:47:21 +1000

    Update jenkins-select2.js

--- jenkins-select2.js
+++ jenkins-select2.js
@@ -6,6 +6,21 @@
     script.onload = function() {
         var $ = window.jQuery;
         $("select").select2()
+
+        // a little bit of re-styling for prod
+        if (window.location.hostname.indexOf("ext.nat") > -1) {
+            $("#header").css("background", "#FA3535");
+            $("#header").append('<div id="prodBox" style="float: right;font-size: 18px;font-weight: bold;margin-top: 10px;margin-right: 10px;color: white;">PROD: SERIOUS</div>')
+            blinker("#prodBox")
+
+            function blinker(id){
+                $(id).fadeOut(500, function(){
+                    $(id).fadeIn(500, function(){
+                        blinker(id);
+                    })
+                });
+            }
+        };
     };
     document.getElementsByTagName("head")[0].appendChild(script);
 })();
